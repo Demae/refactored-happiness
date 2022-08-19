d6c7d10da1d0346d6af623b4c95a75d563fc2b82
Demae <mystiicinc@gmail.com>
2022-08-19 17:48:27 +0930

    Added video play after pressing enter

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 var findElement = document.getElementById.bind(document);
 
-function expandDong() {
+function removeText() {
+  findElement("main").innerHTML = ``;
+}
+
+function revealText() {
   findElement("main").innerHTML = `
     <h1 style="font-family:verdana">femboys.tv</h1>
     <p style="font-family:'Courier New'; margin-bottom:0;">made to fuck with your head</p>
     <p style="font-family:'Courier New'; margin:0; padding-top:5px; color: #555">a scrumptious web project from your neighbourhood haxor</p>
   `;
+}
+
+function playVideo()
+{
+  document.getElementById("video").style.display = "";
+  document.getElementById("video").play();
+
+  removeText();
+
+  setTimeout(function() {
+    revealText();
+  }, 2000);
 }
